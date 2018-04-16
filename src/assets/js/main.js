f26bed4c081d15a82ccfe0dd45a4773cbff67b57
Omer Khushnood <omer.khushnood@gmail.com>
2018-04-16 14:43:18 +0500

    Adds indicator toggle to apply on all charts.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,6 +12,21 @@ var _tc = angular.module('tickercoin', []);
 // Prod API host.
 _tc.constant('API_HOST', 'http://169.60.157.140:3001');
 
+_tc.run([
+
+            '$rootScope',
+    function($rootScope){
+
+        $rootScope.globalChartSettings = {
+
+            indicator: {
+
+                applyToAll: true
+            }
+        };
+    }
+]);
+
 //
 // Ticker coin api service.
 _tc.factory('tickerCoinSrvc', [
@@ -73,8 +88,8 @@ _tc.factory('tickerCoinSrvc', [
 // Any chart directive.
 _tc.directive('tcAnyChart', [
 
-            'tickerCoinSrvc',
-    function(tickerCoinSrvc){
+            'tickerCoinSrvc','$rootScope',
+    function(tickerCoinSrvc , $rootScope){
 
         var _DEFAULT_SINCE_DATE = new Date();
         _DEFAULT_SINCE_DATE.setDate(_DEFAULT_SINCE_DATE.getDate()-1);
@@ -105,17 +120,38 @@ _tc.directive('tcAnyChart', [
             {value: 'stochastic', label: 'Stochastic'},
         ];
 
+        function _getSummedUpVolume(OHLCVData){
+
+            var volume = 0;
+            if(!angular.isArray(OHLCVData) || !OHLCVData.length)
+                return 0;
+            
+            angular.forEach(OHLCVData, function(dataUnit){
+
+                if(angular.isArray(dataUnit) && dataUnit.length >= 5)
+                    volume += dataUnit[4] || 0;
+            });
+
+            return volume;
+        }
+
         return {
 
             template: '' +
                 '<div>' +
                 '   <div ng-class="{\'tc-loader\': !plot.mapping}" ng-repeat="plot in plots">' +
                 '       <h4></h4>' +
-                '       <span>Indicator: </span>' +
-                '       <select ng-model="plot.indicator" ng-change="applyIndicator(plot)">' +
-                '           <option value="{{ind.value}}" ng-repeat="ind in indicators">{{ind.label}}</option>' +
-                '       </select>' +
-                '       <span>Volume: {{plot.volume}}</span>' +
+                '       <div>' +
+                '           <div style="float:right;">Volume: <b>{{plot.volume || \'--\'}}</b></div>' +
+                '           <span>Apply indicator to all: </span>' +
+                '           <input type="checkbox" ng-model="globalChartSettings.indicator.applyToAll">' +
+                '           <br>' +
+                '           <span>Indicator: </span>' +
+                '           <select ng-model="plot.indicator" ng-change="applyIndicator(plot)">' +
+                '               <option value="">-- Select --</option>' +
+                '               <option value="{{ind.value}}" ng-repeat="ind in indicators">{{ind.label}}</option>' +
+                '           </select>' +
+                '       </div>' +
                 '       <div id="{{plot.id}}" style="height: 450px;width: 100%;"></div>' +
                 '   </div>' +
                 '</div>'
@@ -129,29 +165,31 @@ _tc.directive('tcAnyChart', [
 
                 var pair = scope.pair;
                 scope.indicators = techIndicators;
+                scope.globalChartSettings = $rootScope.globalChartSettings;
 
+                function _applyIndicator(plot, indicator){
 
-
-
-
-
-
-
-
+                    plot.chart.plot(0)[indicator || plot.indicator](plot.mapping, undefined, 'line');
+                    plot.indicator = indicator;
+                }
+                
                 scope.applyIndicator = function(plot){
 
-                    plot.chart.plot(0)[plot.indicator](plot.mapping, undefined, 'line');
+                    if(scope.globalChartSettings.indicator.applyToAll)
+                        $rootScope.$broadcast('apply-indicator.tc', plot.indicator);
+                    else
+                        _applyIndicator(plot, plot.indicator);
                 };
 
+                // Apply indicator to all plots.
+                scope.$on('apply-indicator.tc', function(e, indicator){
 
+                    angular.forEach(scope.plots, function(plot){
 
-
-
-
-
-
-
-
+                        _applyIndicator(plot, indicator);
+                    });
+                });
+                
                 scope.plots = [];
                 angular.forEach(pair.presence, function(exchange){
 
@@ -162,29 +200,14 @@ _tc.directive('tcAnyChart', [
                         id: pair.symbol + '-' + exchange + '-' + Date.now(),
                         chart: anychart.stock(),
                         mapping: null,
-                        indicator: 'sma'
+                        indicator: ''
                     });
                 });
-
-
-
-
-
-
-
-
+                
                 var OHLCVOptions = angular.copy(scope.ccxtOptions);
                 OHLCVOptions.since = OHLCVOptions.since.getTime();
                 OHLCVOptions.timeframe = (Math.abs(Math.round(OHLCVOptions.timeframe.amount))||0) + (OHLCVOptions.timeframe.unit||'m');
-
-
-
-
-
-
-
-
-
+                
                 angular.forEach(scope.plots, function(plot){
 
                     tickerCoinSrvc.fetchOHLCVData(plot.exchange, plot.market, OHLCVOptions)
@@ -202,11 +225,9 @@ _tc.directive('tcAnyChart', [
                             mapping.addField('volumn', 5, 'volume');
 
                             plot.mapping = mapping;
-                            plot.volume = OHLCVData.length?OHLCVData[OHLCVData.length-1][4]:0;
+                            plot.volume = _getSummedUpVolume(OHLCVData);
 
                             plot.chart.plot(0).candlestick(mapping).name('ohlc');
-
-                            scope.applyIndicator(plot);
 
                             plot.chart.title(plot.exchange);
 
